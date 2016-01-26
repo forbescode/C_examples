3cee514e0bb2c9d54d4cc2b42b7d162f534791c9
forbescode <ken.forbes@gmail.com>
2016-01-26 14:02:09 -0600

    Change text color in C

---
+++ textcolor.c
@@ -0,0 +1,17 @@
+/* Change text color to 1 of 16 colors
+ *
+ * 16 color codes are 0 to 15
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    system("COLOR 6");
+
+    printf("My Color Text!!!!!!!!!!!!!!");
+
+    return(0);
+
+}
